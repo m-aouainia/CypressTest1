776cafcee8a5ed3122b095f49ee61cf433664ec9
m-aouainia <m.aouainia@gmx.de>
2022-09-20 16:01:26 +0200

    change for mocha4

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+      });
+
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
     },
   },
 });
